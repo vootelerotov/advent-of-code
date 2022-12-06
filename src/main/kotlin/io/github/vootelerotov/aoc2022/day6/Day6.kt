d5b9b64d6de0a257b1c236b9b15b2f8343143fd6
Vootele Rotov <vootele.rotov@gmail.com>
2022-12-06 07:16:13 +0200

    Add solution to day 6

---
+++ src/main/kotlin/io/github/vootelerotov/aoc2022/day6/Day6.kt
@@ -0,0 +1,19 @@
+package io.github.vootelerotov.aoc2022.day6
+
+import io.github.vootelerotov.util.readResourceLines
+
+fun main() {
+  val (message) = readResourceLines("aoc2022/day6/input.txt")
+
+  // 1st
+  message.withIndex().windowed(4).first { it.map(::value).let(::isDistinct)}.last().let(::nth).let(::println)
+
+  // 1st
+  message.withIndex().windowed(14).first { it.map(::value).let(::isDistinct)}.last().let(::nth).let(::println)
+
+}
+
+fun isDistinct(list: List<Char>) = list.distinct().size == list.size
+
+fun <T> nth(indexedValue: IndexedValue<T>) = indexedValue.index + 1
+fun <T> value(indexedValue: IndexedValue<T>) = indexedValue.value
